e0e72f4251e2f9e4ad69ff24feb7607f13c5b1cc
David Teague <71990819+DaveBullet1050@users.noreply.github.com>
2025-03-28 16:15:49 +1300

    all required dmd scripts now in /usr/bin

--- usr/bin/dmd-helper/es-store-last-event.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Stores the event type (game or sysrem) plus full path to game ROM or system name event
-
-echo "${progname}es-store-last-event:Storing script: ${1}, system: ${2}, event: ${3}" >> ${esScriptLogFile}
-
-echo "${1}":"${2}":"${3}" > ${lastEventFile}
-
-echo $BASHPID > ${selectedScriptPidFile}
